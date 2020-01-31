ffdec82b87ee59dae2ca3fb57c896d174a22ca6e
KeithSloan <keith@sloan-home.co.uk>
2020-01-31 04:55:25 +0000

    I2

--- cqgdml.py
+++ cqgdml.py
@@ -7,11 +7,18 @@ class gdml() :
 
     def gObject(solid,position,rotation,material) :
 
-    def gVol(name) :    
+    class gVol(name) :    
+
+       def __init__(self,name) :
+           self.Object = []
+           self.Name = name
+       
+       def add(obj) :
+           append(self.Object(obj))
+
+       def export(name) :    
 
     def gMaterial(name) :    
 
-    def gExport(name) :    
-        
     def gBox(x,y,z) :
         return(BRepPrimAPI_MakeBox(x,y,z).Shape()

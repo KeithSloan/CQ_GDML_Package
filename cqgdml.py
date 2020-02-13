51c787ff237d79aac50a885055018c6435d00b61
KeithSloan <keith@sloan-home.co.uk>
2020-02-13 19:16:04 +0000

    For Question

--- cqgdml.py
+++ cqgdml.py
@@ -5,12 +5,19 @@ class gVol:
           self.Objects = []
           self.SubVols = []
           self.Name = name
-       
+
+      def show(self) :
+          for i in SubVols :
+              i.show
+          for i in self.Objects :
+              i.show
+              
       def addObject(self,obj) :
-          append(self.Objects(obj))
+          objects = []
+          objects.tappend(obj)
 
       def addVol(self,vol) :
-          append(self.SubVols(vols))
+          SubVols.append(vols)
 
       def export(self,name) :
           for v in self.SubVols :
@@ -28,6 +35,12 @@ class gMaterial:
 class gObject:
       def __init__(self,solid,material,position,rotation):
           self.Solid = solid
+          self.Material = material
+          self.Position = position
+          self.rotation = rotation
+
+      def show():
+          self.Solid.show(self.Position, self.Rotation)
 
 class gBox :
       def __init__(self,x,y,z) :
@@ -35,3 +48,7 @@ class gBox :
            self.y = y
            self.z = z
            #self.shape = BRepPrimAPI_MakeBox(x,y,z).Shape()
+
+      def show(position, rotation) :
+          shape = BRepPrimAPI_MakeBox(x,y,z).Shape()
+          show_object(shape)
